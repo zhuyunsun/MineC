925684e12998be5cc49d6dc0e5819c7abf136b4f
zhuyun <1935589833@qq.com>
2020-12-22 10:48:34 +0800

    Initial commit

---
+++ MineC/basics.c
@@ -0,0 +1,21 @@
+//
+//  basics.c
+//  MineC
+//
+//  Created by 朱运 on 2020/12/22.
+//
+
+#include "basics.h"
+
+int maxCout(int a,int b){
+    if (a < b) {
+        return b;
+    }
+    return a;
+}
+int allFiles(void){
+    
+    
+    
+    return 1;
+}
---
+++ MineC/basics.h
@@ -0,0 +1,18 @@
+//
+//  basics.h
+//  MineC
+//
+//  Created by 朱运 on 2020/12/22.
+//
+
+#ifndef basics_h
+#define basics_h
+
+#include <stdio.h>
+
+/// 返回2个整数中最大的整数
+int maxCout(int a,int b);
+/// 某个路径下的所有文件
+int allFiles(void);
+
+#endif /* basics_h */
